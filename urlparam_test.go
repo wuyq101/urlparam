fc96bf8575e5066db65c56b92a6e2a2513e2728d
Hchen <928572361@qq.com>
2017-05-27 14:51:30 +0800

    Update urlparam_test.go

--- urlparam_test.go
+++ urlparam_test.go
@@ -3,19 +3,26 @@ package urlparam
 import (
 	"net/url"
 	"testing"
+	"time"
 )
 
 type TestObj struct {
-	Id    int    `url:"id"`
-	AppId int64  `url:"app_id"`
-	Name  string `url:"name"`
+	Code  string `url:"code" regex:"^[a-zA-Z0-9 .]+$" required:"true"`
+	Date  time.Time `url:"date" layout:"2006-01-02" required:"true"`
+	Datetime time.Time `url:"datetime" layout:"200601021504" required:"true"`
+	Number  int `url:"number" required:"true"`
+	Parameter  string `url:"parameter" list:"param1,param2,param3" required:"true"`
 }
 
 func TestMarshal(t *testing.T) {
+	date, _ := time.Parse("2006-01-02", "2017-05-26")
+	datetime, _ := time.Parse("200601021504","201705261503")
 	obj := &TestObj{
-		Id:    1,
-		AppId: 1001,
-		Name:  "test app",
+		Code: "123",
+		Date: date,
+		Datetime:  datetime,
+		Number: 454,
+		Parameter: "param1",
 	}
 	params := Marshal(obj)
 	t.Logf("params %v", params)
@@ -24,13 +31,16 @@ func TestMarshal(t *testing.T) {
 
 func TestUnmarshal(t *testing.T) {
 	params := make(url.Values)
-	params.Set("id", "1")
-	params.Set("app_id", "1001")
-	params.Set("name", "test app")
+	params.Set("code", "123")
+	params.Set("date", "2017-05-25")
+	params.Set("datetime", "201705261504")
+	params.Set("number", "16983")
+	params.Set("parameter", "param1")
 	var obj TestObj
 	err := Unmarshal(params, &obj)
 	if err != nil {
 		t.Fatal("unmarshal error: %v", err)
 	}
 	t.Logf("result %+v", obj)
+	t.Logf("params %+v", params)
 }
